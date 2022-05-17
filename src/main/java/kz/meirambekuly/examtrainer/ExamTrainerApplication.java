b8fe6d09d85e5a5295a8254f5323e4d8a13380f3
yerzat <erzatnis@gmail.com>
2022-05-17 17:42:13 +0600

    first commit

---
+++ src/main/java/kz/meirambekuly/examtrainer/ExamTrainerApplication.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.examtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamTrainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/meirambekuly/examtrainer/ExamTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.examtrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTrainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
