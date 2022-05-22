3514096e8713e7da77d1491a55db48cbc804a54d
yerzat <erzatnis@gmail.com>
2022-05-22 15:20:05 +0600

    added new dependency

--- src/main/java/kz/meirambekuly/examtrainer/config/UaaConfiguration.java
+++ src/main/java/kz/meirambekuly/examtrainer/config/UaaConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,18 +50,16 @@ public class UaaConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui.html#/");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
-                .exceptionHandling()
-                .authenticationEntryPoint(((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
-                .and()
                 .csrf().disable()
                 .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(uaaWebSecurityConfiguration, UsernamePasswordAuthenticationFilter.class)
-                .headers()
-                .frameOptions()
-                .disable()
-                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -72,7 +71,7 @@ public class UaaConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**", "/swagger-ui.html#/**").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/account/**").authenticated()
                 .antMatchers("/api/question/**").authenticated()
                 .antMatchers("/api/subject/**").authenticated()
